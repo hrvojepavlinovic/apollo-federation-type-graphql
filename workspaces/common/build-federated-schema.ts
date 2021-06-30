d0629fa39a8a351e95020bcad71f4d566c365b36
Hrvoje Pavlinovic <hrvoje.pavlinovic@rimac-automobili.com>
2021-06-30 21:44:36 +0200

    Initial commit

---
+++ workspaces/common/build-federated-schema.ts
@@ -0,0 +1,36 @@
+import {
+  buildFederatedSchema as buildApolloFederationSchema,
+  printSchema,
+} from '@apollo/federation'
+import federationDirectives from '@apollo/federation/dist/directives'
+import type { GraphQLResolverMap } from 'apollo-graphql'
+import { addResolversToSchema } from 'apollo-graphql'
+import { gql } from 'apollo-server'
+import { specifiedDirectives } from 'graphql'
+import type { BuildSchemaOptions } from 'type-graphql'
+import {
+  buildSchema,
+  createResolversMap,
+} from 'type-graphql'
+
+export const buildFederatedSchema = async (
+  options: Omit<BuildSchemaOptions, 'skipCheck'>,
+  referenceResolvers?: GraphQLResolverMap,
+) => {
+  const schema = await buildSchema({
+      ...options,
+      directives: [...specifiedDirectives, ...federationDirectives, ...(options.directives ?? [])],
+      skipCheck: true,
+  })
+
+  const federatedSchema = buildApolloFederationSchema({
+      resolvers: createResolversMap(schema) as never,
+      typeDefs: gql(printSchema(schema)),
+  })
+
+  if (referenceResolvers) {
+      addResolversToSchema(federatedSchema, referenceResolvers)
+  }
+
+  return federatedSchema
+}
